23a57c5b7550ef4d89e1a418b7be98a724d9415c
mdsifat <ibnsifat@gmail.com>
2023-03-08 20:57:41 +0600

    update

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(cors())
 app.use(express.json())
 app.use('/user',userRoute)
 
+app.use('/',(req,res)=>{
+    res.send('deployed')
+})
 app.listen(port,()=>{
     console.log('running on port 5000')
 })
